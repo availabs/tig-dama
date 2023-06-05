bdd1d651141560a9ddcc6355a2bbdce1e0556e2c
sarangp3010 <spatel25@albany.edu>
2023-06-05 11:53:17 -0400

    changes

--- src/Routes.js
+++ src/Routes.js
@@ -6,7 +6,7 @@ import Documentation from "./pages/Documentation"
 
 const Routes = [
   Auth,
-  ...DataManager('','tig_dama_dev'),
+  ...DataManager('' /*, 'tig_dama_dev' */),
   ...Documentation,
   NoMatch
 ]
--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,11 @@ import {  } from "~/modules/avl-components/src";
 import { Reducers, messages } from "@availabs/ams";
 import { configureStore } from "@reduxjs/toolkit";
 
-import data_manager from "~/pages/DataManager/store";
+// import data_manager from "~/pages/DataManager/store";
 
 export default configureStore({
   reducer: {
-    data_manager,
+    // data_manager,
     ...Reducers,
     messages,
   },
