2a279ad6cb677dd74cc17dd270bc9a1bdbef15e1
alex <am3081@gmail.com>
2023-06-22 14:22:15 -0400

    changes

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const PROJECT_NAME = "NPMRDS";
+export const PROJECT_NAME = "TIG";
 
 let API_HOST = "https://tig22.nymtc.org/graph/"; //"https://graph.availabs.org/";
 let AUTH_HOST = "https://availauth.availabs.org";
@@ -7,10 +7,10 @@ let DAMA_HOST = "https://tig22.nymtc.org/dama";
 
 if (process.env.NODE_ENV === "development") {
   // API_HOST = "https://graph.availabs.org";
-  API_HOST =  "http://localhost:4444";
+  //API_HOST =  "http://localhost:4444";
   CLIENT_HOST = "localhost:3000";
   //DAMA_HOST = "https://dama-dev.availabs.org";
-  DAMA_HOST = "http://localhost:3369";
+  //DAMA_HOST = "http://localhost:3369";
 }
 
 export { API_HOST, AUTH_HOST, CLIENT_HOST, DAMA_HOST };
