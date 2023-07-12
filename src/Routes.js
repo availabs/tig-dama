21a4f0f19a9948470815a714e2a4edfa399ff1ea
Eric Conklin <eric.s.conklin@gmail.com>
2023-07-12 10:23:04 -0400

    stuff

--- src/Routes.js
+++ src/Routes.js
@@ -8,8 +8,8 @@ import List from '~/pages/TigDatamanager/Source/list'
 
 const Routes = [
   Auth,
-  ...DataManager('','tig_dama_dev',false, {List}),
-  // ...DataManager('','pan',false, {List}),
+  // ...DataManager('','tig_dama_dev',false, {List}),
+  ...DataManager('','pan',false, {List}),
   ...Documentation,
   NoMatch
 ]
--- src/config.js
+++ src/config.js
@@ -7,11 +7,11 @@ let DAMA_HOST = "https://tig22.nymtc.org/dama";
 
 if (process.env.NODE_ENV === "development") {
   // API_HOST = "https://graph.availabs.org";
-  // API_HOST =  "http://localhost:4445";
+  API_HOST =  "http://localhost:4445";
   CLIENT_HOST = "localhost:3000";
-  // DAMA_HOST = "https://localhost:3369"
+  DAMA_HOST = "http://localhost:3369"
   // DAMA_HOST = "https://dama-dev.availabs.org";
-  
+
 }
 
 export { API_HOST, AUTH_HOST, CLIENT_HOST, DAMA_HOST };
