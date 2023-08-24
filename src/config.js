6637c9fbe6ec7f27bf27d7c058914a4a96b0e6aa
Eric Conklin <eric.s.conklin@gmail.com>
2023-08-24 09:38:53 -0400

    stuff

--- src/config.js
+++ src/config.js
@@ -9,10 +9,11 @@ let PG_ENV = 'tig_dama_dev'
 if (process.env.NODE_ENV === "development") {
 
   API_HOST =  "http://localhost:4444";
-  DAMA_HOST = "http://localhost:4444";
-  // DAMA_HOST = "http://localhost:3369"
+  // DAMA_HOST = "http://localhost:4444";
+  DAMA_HOST = "http://localhost:3369";
   // DAMA_HOST = "https://dama-dev.availabs.org";
-  PG_ENV = 'pan'
+  PG_ENV = 'pan';
+  // PG_ENV = "freight_data";
   CLIENT_HOST = "localhost:5173";
 }
 
