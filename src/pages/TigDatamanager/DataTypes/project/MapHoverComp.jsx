336c456eaf7520ecf0664c15959568d34bd042c0
Ryan Dubowsky <r.k.dubowsky@gmail.com>
2023-11-27 15:02:38 -0500

    Added link to table from Map

--- src/pages/TigDatamanager/DataTypes/project/MapHoverComp.jsx
+++ src/pages/TigDatamanager/DataTypes/project/MapHoverComp.jsx
@@ -1,9 +1,10 @@
 import React, {useMemo} from 'react'
+import {Link, useNavigate, useSearchParams} from 'react-router-dom'
 import { DamaContext } from "~/pages/DataManager/store"
 import get from 'lodash/get'
 
 const TipHoverComp = ({ data, layer }) => {
-  const { source: { type }, attributes, activeViewId, props: { filters, activeView: {metadata: { years } } }  } = layer
+  const { source: { source_id }, attributes, activeViewId, props: { filters, activeView: {metadata: { years } } }  } = layer
 
   const { pgEnv, falcor, falcorCache  } = React.useContext(DamaContext)
   const id = React.useMemo(() => get(data, '[0]', null), [data])
@@ -38,25 +39,24 @@ const TipHoverComp = ({ data, layer }) => {
   }, [id, falcorCache, activeViewId, pgEnv]);
 
 
-
   const cols = attrInfo['rtp_id'] ? [
     {col:'rtp_id', name: 'Project Id', display: d => d},
+    {col:'rtp_id', name: 'Link to table', display: d => <LinkRow feature={d} sourceId={source_id} />},
     {col:'year', name: 'Year', display: d => d},
     {col:'plan_portion', name: 'Plan Portion', display: d => d},
     {col:'sponsor', name: 'Sponsor', display: d => d},
     {col:'ptype', name: 'Project Type', display: d => d},
     {col:'cost', name: 'Cost', display: d => d && d !== 'null' ? `$${d}M` : ''},
-    
   ] : [
     {col:'tip_id', name: 'Project Id', display: d => d},
+    {col:'tip_id', name: 'Link to table', display: d => <LinkRow feature={d} sourceId={source_id} />},
     {col:'sponsor', name: 'Sponsor', display: d => d},
     {col:'mpo', name: 'MPO', display: d => d},
     {col:'ptype', name: 'Project Type', display: d => d},
     {col:'cost', name: 'Cost', display: d => d ? `$${d}M` : ''},
-    
   ]
 
- 
+  
 
   return (
     <div className='bg-white px-4 py-2 max-w-[300px] scrollbar-xs overflow-y-scroll'>
@@ -77,6 +77,23 @@ const TipHoverComp = ({ data, layer }) => {
     </div>
   )
 }
+
+const LinkRow = ({ feature, sourceId }) => {
+  const navigate = useNavigate()
+  const [searchParams] = useSearchParams();
+  const variable = searchParams.get("variable");
+  return (
+    <Link
+      onClick={(e) =>
+        navigate(
+          `/source/${sourceId}/table?variable=${variable}&featureId=${feature}`
+        )
+      }
+    >
+      Link to table
+    </Link>
+  );
+};
 
 
 export default TipHoverComp
