476904ea7330af7a75db2b9623201a90badc4315
Ryan Dubowsky <r.k.dubowsky@gmail.com>
2024-01-09 10:41:56 -0500

    Added projectType, county, sponsor, mpo, and planportion filters to TIP/RTP table
    Added currency formatting to cost column in table

--- src/pages/TigDatamanager/DataTypes/project/TableFilterComp.jsx
+++ src/pages/TigDatamanager/DataTypes/project/TableFilterComp.jsx
@@ -4,26 +4,58 @@ import download from "downloadjs"
 import { Button } from "~/modules/avl-components/src"
 
 
+function onlyUnique(value, index, array) {
+  return array.indexOf(value) === index;
+}
+
+function alphaSort(a, b) {
+  if (a < b) {
+    return -1;
+  } else if (b < a) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
 const ProjectTableFilter = (
   { source, filters, setFilters, data }
 ) => {
   const projectIdFilterValue = filters["projectId"]?.value || null;
+  const projectTypeFilterValue = filters["ptype"]?.value || null;
+  const countyFilterValue = filters['county']?.value || null;
+  const sponsorFilterValue = filters['sponsor']?.value || null;
+  const mpoFilterValue = filters['mpo']?.value || null; //only for RTP
+  const planPortionFilterValue = filters['plan_portion']?.value || null; //only for TIP
+
   let projectKey = source?.name?.includes("RTP") ? "rtp_id" : "tip_id";
 
   const [searchParams] = useSearchParams();
-  const featureId = searchParams.get("featureId")
+  const urlFeatureId = searchParams.get("featureId")
 
   React.useEffect(() => {
     if (!projectIdFilterValue) {
-      if (featureId) {
+      if (urlFeatureId) {
         setFilters({
-          projectId: { value: featureId },
+          projectId: { value: urlFeatureId },
         });
       }
     }
   }, []);
 
-  const allProjectIds = data.map(d => d[projectKey]);
+  const allProjectIds = data.map(d => d[projectKey]).filter(onlyUnique);
+  const allProjectTypes = data.map(d => d['ptype']).filter(onlyUnique);
+  const allCounties = data.map(d => d.county).filter(onlyUnique);
+  const allSponsors = data.map(d => d.sponsor).filter(onlyUnique);
+  const allPlanPortions = data.map(d => d.plan_portion).filter(onlyUnique);
+  const allMpo = data.map(d => d.mpo).filter(onlyUnique);
+
+
+  allProjectIds.sort(alphaSort);
+  allProjectTypes.sort(alphaSort);
+  allCounties.sort(alphaSort);
+  allSponsors.sort(alphaSort);
+  allMpo.sort(alphaSort);
 
   const columns = source?.metadata?.columns;
   const downloadColumns = columns.filter((c) => c.name !== "wkb_geometry");
@@ -48,8 +80,8 @@ const ProjectTableFilter = (
     <div className="flex flex-1 border-blue-100">
       <div className='flex flex-1'>
         <div className='flex-1' /> 
-        <div className="py-3.5 px-2 text-sm text-gray-400">Project ID: </div>
-        <div className="px-2">
+        <div className="py-3.5 px-1 text-sm text-gray-400">{projectKey}</div>
+        <div className="px-1">
           <select
             className="pl-3 pr-4 py-2.5 border border-blue-100 bg-blue-50 w-full bg-white mr-2 flex items-center justify-between text-sm"
             value={projectIdFilterValue || ""}
@@ -61,6 +93,116 @@ const ProjectTableFilter = (
               None
             </option>
             {allProjectIds.map((k, i) => (
+              <option key={i} className="ml-2  truncate" value={k}>
+                {k}
+              </option>
+            ))}
+          </select>
+        </div>
+      </div>
+      <div className='flex flex-1'>
+        <div className='flex-1' /> 
+        <div className="py-3.5 px-1 text-sm text-gray-400">Project Type:</div>
+        <div className="px-1">
+          <select
+            className="pl-3 pr-4 py-2.5 border border-blue-100 bg-blue-50 w-full bg-white mr-2 flex items-center justify-between text-sm"
+            value={projectTypeFilterValue || ""}
+            onChange={(e) =>
+              setFilters({ ptype: { value: e.target.value } })
+            }
+          >
+            <option className="ml-2  truncate" value={""}>
+              None
+            </option>
+            {allProjectTypes.map((k, i) => (
+              <option key={i} className="ml-2  truncate" value={k}>
+                {k}
+              </option>
+            ))}
+          </select>
+        </div>
+      </div>
+      {projectKey === "tip_id" && <div className='flex flex-1'>
+        <div className='flex-1' /> 
+        <div className="py-3.5 px-1 text-sm text-gray-400">MPO:</div>
+        <div className="px-1">
+          <select
+            className="pl-3 pr-4 py-2.5 border border-blue-100 bg-blue-50 w-full bg-white mr-2 flex items-center justify-between text-sm"
+            value={mpoFilterValue || ""}
+            onChange={(e) =>
+              setFilters({ mpo: { value: e.target.value } })
+            }
+          >
+            <option className="ml-2  truncate" value={""}>
+              None
+            </option>
+            {allMpo.map((k, i) => (
+              <option key={i} className="ml-2  truncate" value={k}>
+                {k}
+              </option>
+            ))}
+          </select>
+        </div>
+      </div>}
+      {projectKey === "rtp_id" && <div className='flex flex-1'>
+        <div className='flex-1' /> 
+        <div className="py-3.5 px-1 text-sm text-gray-400">Plan Portion:</div>
+        <div className="px-1">
+          <select
+            className="pl-3 pr-4 py-2.5 border border-blue-100 bg-blue-50 w-full bg-white mr-2 flex items-center justify-between text-sm"
+            value={planPortionFilterValue || ""}
+            onChange={(e) =>
+              setFilters({ plan_portion: { value: e.target.value } })
+            }
+          >
+            <option className="ml-2  truncate" value={""}>
+              None
+            </option>
+            {allPlanPortions.map((k, i) => (
+              <option key={i} className="ml-2  truncate" value={k}>
+                {k}
+              </option>
+            ))}
+          </select>
+        </div>
+      </div>}
+      <div className='flex flex-1'>
+        <div className='flex-1' /> 
+        <div className="py-3.5 px-1 text-sm text-gray-400">Sponsor:</div>
+        <div className="px-1">
+          <select
+            className="pl-3 pr-4 py-2.5 border border-blue-100 bg-blue-50 w-full bg-white mr-2 flex items-center justify-between text-sm"
+            value={sponsorFilterValue || ""}
+            onChange={(e) =>
+              setFilters({ sponsor: { value: e.target.value } })
+            }
+          >
+            <option className="ml-2  truncate" value={""}>
+              None
+            </option>
+            {allSponsors.map((k, i) => (
+              <option key={i} className="ml-2  truncate" value={k}>
+                {k}
+              </option>
+            ))}
+          </select>
+        </div>
+      </div>
+      <div className='flex flex-1'>
+        <div className='flex-1' /> 
+        <div className="py-3.5 px-1 text-sm text-gray-400">County:</div>
+        <div className="px-1">
+          <select
+            className="pl-3 pr-4 py-2.5 border border-blue-100 bg-blue-50 w-full bg-white mr-2 flex items-center justify-between text-sm"
+            value={countyFilterValue || ""}
+            onChange={(e) =>
+              setFilters({ county: { value: e.target.value } })
+            }
+          >
+            <option className="ml-2  truncate" value={""}>
+              None
+            </option>
+            {allCounties.map((k, i) => (
               <option key={i} className="ml-2  truncate" value={k}>
                 {k}
               </option>
@@ -87,19 +229,30 @@ export const ProjectTableTransform = (tableData, attributes, filters, years, sou
   const activeFilterKeys = Object.keys(filters).filter(
     (filterKey) => !!filters[filterKey].value
   );
-
+console.log("tabledata", tableData)
   filteredData = tableData.filter((val) => {
     const shouldKeep = activeFilterKeys.every((filterKey) => {
-      return filters[filterKey].value === val[projectKey];
+      const dataAccessor = filterKey === 'projectId' ? projectKey : filterKey;
+      return filters[filterKey].value === val[dataAccessor];
     });
     return shouldKeep;
   });
 
-  const columns = attributes
-    .map((attr) => ({
-      Header: attr,
-      accessor: attr,
-    }));
+  const columns = attributes.map((attr) => {
+    if (attr === "cost") {
+      return {
+        Header: attr,
+        accessor: attr,
+        id: attr,
+        Cell: ({ value }) => {
+          const displayValue = value !== "null" ? `$${parseInt(value).toFixed(2).toLocaleString()}` : "--"
+          return <div>{displayValue}</div>;
+        },
+      };
+    } else {
+      return { Header: attr, accessor: attr, id: attr };
+    }
+  });
 
   columns.push({
     Header: "Actions",
