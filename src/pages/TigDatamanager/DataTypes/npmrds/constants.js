2713d39d79b1036809ba022530d3f8e84af32f58
Ryan Dubowsky <r.k.dubowsky@gmail.com>
2024-04-22 13:56:46 -0400

    draws grey npmrds lines!

--- src/pages/TigDatamanager/DataTypes/npmrds/constants.js
+++ src/pages/TigDatamanager/DataTypes/npmrds/constants.js
@@ -1,10 +1,9 @@
-const finishYear = 2020;
-const startYear = 2007
+const finishYear = 2021;
+const startYear = 2016
 
 export const NPMRDS_ATTRIBUTES = {
   year: {
     values: [
-      "all",
       ...Array.from(
         { length: finishYear - startYear },
         (_, i) => startYear + 1 + i
@@ -14,9 +13,15 @@ export const NPMRDS_ATTRIBUTES = {
     tableFilter: true,
     filterRemovable: true,
   },
+  hour: {
+    values: Array.from({ length: 24 }, (_, i) => -1 + 1 + i),
+    type: "range",
+    mapFilter: true,
+    tableFilter: true,
+    filterRemovable: false,
+  },
   month:{
     values: [
-      "all",
       ...Array.from(
         { length: 12 },
         (_, i) => 0 + 1 + i
@@ -24,8 +29,8 @@ export const NPMRDS_ATTRIBUTES = {
     ],
     mapFilter: true,
   },
-  geoids:{
-    values:[1001],
+  geoid:{
+    values:[34003,34013,34017,34019,34021,34023,34025,34027,34029,34031,34035,34037,34039,36005,36027,36047,36059,36061,36071,36079,36081,36085,36087,36103,36105,36111,36119,"09009","09005","09001"],
     mapFilter: true,
   }
 }
--- src/pages/TigDatamanager/DataTypes/npmrds/map/npmrdsMapFilter.jsx
+++ src/pages/TigDatamanager/DataTypes/npmrds/map/npmrdsMapFilter.jsx
@@ -1,105 +1,247 @@
 import {useEffect, useContext} from "react";
+import cloneDeep from 'lodash/cloneDeep'
+import isEqual from 'lodash/isEqual'
 import { NPMRDS_ATTRIBUTES } from "../constants";
+import * as d3scale from "d3-scale"
+import { useSearchParams } from "react-router-dom";
 import { DamaContext } from "~/pages/DataManager/store";
-
+import get from "lodash/get";
 import {
   falcorGraph,
 } from "~/modules/avl-components/src"
 
+const LEGEND_RANGE = ['rgb(255,0,0)', 'rgb(255,100,0)', 'rgb(255,255,0)', 'rgb(0,100,255)', 'rgb(0,0,255)', 'rgb(0,255,255)', 'rgb(0,255,0)']
+const LEGEND_DOMAIN = [0, 10, 20, 30, 40, 50, 55];
+const npmrdsPaint = {
+  'line-color': '#ccc',
+  'line-width': [
+    "interpolate",
+    ["linear"],
+    ["zoom"],
+    0,
+    [
+      "match",
+      ["get", "n"],
+      [1, 2],
+      0.5,
+      0
+    ],
+    13,
+    [
+      "match",
+      ["get", "n"],
+      [1, 2],
+      1.5,
+      1
+    ],
+    18,
+    [
+      "match",
+      ["get", "n"],
+      [1, 2],
+      8,
+      5
+    ]
+  ],
+  'line-opacity': [
+    "case",
+    ["boolean", ["feature-state", "hover"], false],
+    0.4,
+    1
+  ],
+  'line-offset': {
+    base: 1.5,
+    stops: [[5, 0], [9, 1], [15, 3], [18, 7]]
+  }
+}
+
+const NpmrdsLayers = ['2016','2017','2018','2019','2020','2021']
+  .map(year => {
+    return {
+      id: `tmc-${year}`,
+      type: 'line',
+      source: 'npmrds',
+      beneath: 'waterway-label',
+      'source-layer': `npmrds_${year}`,
+      layout: {
+        'visibility': 'visible',
+        'line-join': 'round',
+        'line-cap': 'round'
+      },
+      paint: npmrdsPaint
+    }
+  })
+
+
+
+const NpmrdsSources = [
+  { id: "npmrds",
+    source: {
+      type: "vector",
+      url: "https://tiles.availabs.org/data/npmrds.json"
+    }
+  }
+]
+const TrafficSignalsSources = [
+  { id: "traffic_signals",
+    source: {
+      type: "vector",
+      url: "https://tiles.availabs.org/data/osm_traffic_signals.json"
+    }
+  }
+]
+const SOURCES = [
+  ...NpmrdsSources,
+  ...TrafficSignalsSources,
+  {
+      id: "geo-boundaries-source",
+      source: {
+          type: "geojson",
+          data: {
+              type: "FeatureCollection",
+              features: []
+          }
+      }
+  },
+]
+const LAYERS = [
+  // ...ConflationLayerCase,
+  ...NpmrdsLayers,
+  // ...TrafficSignalsLayers,
+  {
+      id: "geo-boundaries",
+      type: "line",
+      source: "geo-boundaries-source",
+      paint: {
+          "line-color": "#fff"
+      }
+  }
+]
+
+
 const API_HOST = 'https://tigtest2.nymtc.org/api2/graph'
 const tig_falcor = falcorGraph(API_HOST)
+const GEO_LEVEL = 'COUNTY';
+const npmrdsMapFilter = ({
+  filters,
+  setFilters,
+  filterType = "mapFilter",
+  tempSymbology, 
+  setTempSymbology,
+  activeViewId
+}) => {
 
-
-// export const npmrdsMapFilter = (props) => {
-//   console.log("npmrds map filter props::", props)
-//   const {filters} = props;
-//   // useEffect(() => {
-//   //   // let year = filters.year.value,
-//   //   // month = filters.month.value,
-//   //   // geoids = filters.geography.domain.filter(d => d.name === filters.geography.value)[0].value,
-//   //   // filtered = this.geographies
-//   //   //     .filter(({value}) => geoids.includes(value));
-
-//   //   // // console.log('request', ['tig','npmrds',`${month}|${year}`,filtered.map(d => `${d.geolevel}|${d.value}`), 'data'])
-
-//   //   // let requests = filtered.reduce((a, c) => {
-//   //   //     a.push(['tig', 'npmrds', `${month}|${year}`, `${c.geolevel}|${c.value}`, 'data'])
-//   //   //     a.push(["geo", c.geolevel.toLowerCase(), c.value, "geometry"]);
-//   //   //     return a;
-//   //   // }, [])
-//   //   // return falcor.get(...requests).then(() => this.updateLegendTitle())
-
-//   //   //https://tigtest2.nymtc.org/api2/graph?paths=%5B%5B%22tig%22%2C%22datasources%22%2C%22views%22%2C%22sourceId%22%2C%5B2%2C5%2C7%2C8%2C9%2C45%2C47%2C48%2C49%2C50%2C61%2C64%2C69%2C70%2C71%2C72%2C73%2C74%2C75%2C76%2C77%2C78%2C79%2C88%2C89%2C90%2C91%5D%2C%22length%22%5D%5D&method=get
-//   //   //tigtest2.nymtc.org/api2/graph?paths=
-      
-
-
-//   //   async function getData() {
-//   //     console.log("getting data");
-//   //     //const url = `https://tigtest2.nymtc.org/api2/graph?paths=%5B%5B%22tig%22%2C%22datasources%22%2C%22views%22%2C%22sourceId%22%2C%5B2%2C5%2C7%2C8%2C9%2C45%2C47%2C48%2C49%2C50%2C61%2C64%2C69%2C70%2C71%2C72%2C73%2C74%2C75%2C76%2C77%2C78%2C79%2C88%2C89%2C90%2C91%5D%2C%22length%22%5D%5D&method=get`
-//   //     const res = await fetch(
-//   //       "https://tigtest2.nymtc.org/api2/graph?" +
-//   //         new URLSearchParams({
-//   //           paths: '[["tig", "npmrds", "10|2020", ["county_code|10001"], "data"]]',
-//   //           method: "get",
-//   //         })
-//   //     );
-//   //     const pollingData = await res.json();
-//   //     console.log(pollingData);
-//   //   }
-
-//   //   getData();
-//   // },[filters])
-
-
-
-//   return (
-//     <div>
-//       Hello world
-//     </div>
-//   );
-// }
-
-
-const npmrdsMapFilter = ({ filters, setFilters, filterType = "mapFilter" }) => {
-  const allContext = useContext(DamaContext);
+  const [searchParams] = useSearchParams();
+  const activeDataVersionId = parseInt(searchParams.get("variable")) || activeViewId;
+  let newSymbology = cloneDeep(tempSymbology);
   const year =  filters?.year?.value;
-console.log("npmrds map filter, allContext::", allContext)
+  const month = filters?.month?.value;
+  const hour = filters?.hour?.value;
+  const geoid = filters?.geoid?.value;
   useEffect(() => {
     const newFilters = {...filters};
+    console.log(NPMRDS_ATTRIBUTES)
     if (!year) {
-      newFilters.year = { value: 2019 }
-    }    
+      newFilters.year = { value: NPMRDS_ATTRIBUTES['year'].values[0] }
+    }
+    if (!month){
+      newFilters.month = {value : NPMRDS_ATTRIBUTES['month'].values[0]}
+    }
+    if(!hour){
+      newFilters.hour = {value: NPMRDS_ATTRIBUTES['hour'].values[0]}
+    }
+    if(!geoid){
+      newFilters.geoid = {value: NPMRDS_ATTRIBUTES['geoid'].values[0]}
+    }
     setFilters(newFilters)
   }, []);
 
+  const getColorScale = (domain) => {
+    if (LEGEND_RANGE.length > domain.length) {
+        domain = []
+        return false
+    }
+    domain = [0, 10, 20, 30, 40, 50, 55]
+    //ckmeans(domain,this.legend.range.length).map(d => Math.min(...d))
+    //this.updateLegend(this.filters, this.legend)
+    return d3scale.scaleThreshold()
+        .domain(domain)
+        .range(LEGEND_RANGE);
+}
 
   useEffect(() => {
-
-
-    //https://tigtest2.nymtc.org/api2/graph?paths=%5B%5B%22tig%22%2C%22datasources%22%2C%22views%22%2C%22sourceId%22%2C%5B2%2C5%2C7%2C8%2C9%2C45%2C47%2C48%2C49%2C50%2C61%2C64%2C69%2C70%2C71%2C72%2C73%2C74%2C75%2C76%2C77%2C78%2C79%2C88%2C89%2C90%2C91%5D%2C%22length%22%5D%5D&method=get
-    //tigtest2.nymtc.org/api2/graph?paths=
-      
-
-
-    async function getData() {
-      console.log("getting data");
-      // let year = filters.year.value,
-      // month = filters.month.value,
+    async function getData() {    
       // geoids = filters.geography.domain.filter(d => d.name === filters.geography.value)[0].value,
-      // filtered = this.geographies
-      //     .filter(({value}) => geoids.includes(value));
+      const geoDataPath = ["tig", "npmrds", `${month}|${year}`, `${GEO_LEVEL}|${geoid}`, "data"];
+      const boundingGeometryPath = ["geo", GEO_LEVEL.toLowerCase(), "36061", "geometry"]
+      const requests = [
+        geoDataPath,
+        boundingGeometryPath,
+      ];
+      // console.log({geometries})
+      // console.log({requests})
+      const response = await tig_falcor.get(...requests);
+      const data = get(response, ['json', ...geoDataPath ])
+      console.log("data::", data)
+      console.log("newSymbology", newSymbology)
+      if (!newSymbology?.source) {
+        newSymbology.sources = SOURCES;
+        const source_id = newSymbology?.sources?.[0]?.id || "0";
+        const source_layer = `s${source_id}_v${activeDataVersionId}`;
+        console.log("data inside setting source", data)
 
-      // console.log('request', ['tig','npmrds',`${month}|${year}`,filtered.map(d => `${d.geolevel}|${d.value}`), 'data'])
+        const scale = d3scale.scaleThreshold()
+          .domain(LEGEND_DOMAIN)
+          .range(LEGEND_RANGE)
 
-      let requests = ["tig", "npmrds", "10|2020", ["county_code|10001"], "data"]
-      return tig_falcor.get(requests).then((resp) => {
-        console.log("resp from tig_falcor", resp)
-      })
+        const colors = Object.keys(data).reduce((a, c) => {
+            let val = scale(get(data[c], `s[${hour}]`, 0))
+            a[c] = val ? val : 'hsla(185, 0%, 27%,0.8)'
+            return a;
+        }, {});
+
+        newSymbology.layers = LAYERS.map(layer => ({
+          ...layer,
+          ...(NPMRDS_ATTRIBUTES['year'].values.map(year => ({
+            paint: {
+              "line-color": [
+                "case",
+                ["has", ["to-string", ["get", 'tmc']], ["literal", colors]],
+                ["get", ["to-string", ["get", 'tmc']], ["literal", colors]],
+                "hsla(185, 0%, 27%,0.0)",
+            ],
+              "line-width": 3,
+            }
+          })))
+        }));
+
+        newSymbology.legend =  {
+          type: "threshold",
+          domain: LEGEND_DOMAIN,
+          range: LEGEND_RANGE,
+          format: ",.1f",
+          direction: 'horizontal',
+          show: false,
+          units: "Average Speed (mph)"
+        }
+        console.log(newSymbology)
+
+        if (!isEqual(newSymbology, tempSymbology)) {
+          console.log("setting new newSymbology");
+          setTempSymbology(newSymbology);
+        }
+
+      }
     }
 
-    getData();
+    if(year && month) {
+      getData();
+    }
+
   },[filters])
+
+
+
 
 
   return (
