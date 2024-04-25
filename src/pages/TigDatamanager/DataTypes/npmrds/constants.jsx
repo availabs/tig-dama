4e6bb333b21ef66e19faeea03cc82e5bb67aac16
Ryan Dubowsky <r.k.dubowsky@gmail.com>
2024-04-25 09:53:08 -0400

    changes so that the new input component works
    also added more counties

--- src/pages/TigDatamanager/DataTypes/npmrds/constants.jsx
+++ src/pages/TigDatamanager/DataTypes/npmrds/constants.jsx
@@ -1,6 +1,6 @@
-import moment from "moment"
+import moment from "moment";
 const finishYear = 2021;
-const startYear = 2016
+const startYear = 2016;
 
 export const NPMRDS_ATTRIBUTES = {
   year: {
@@ -16,7 +16,7 @@ export const NPMRDS_ATTRIBUTES = {
   },
   hour: {
     values: Array.from({ length: 24 }, (_, i) => -1 + 1 + i),
-    optionComp: ({val}) => {
+    optionComp: ({ val }) => {
       const lowerStringHour = moment(val, "HH").format("HH:mm");
       const upperStringHour = moment(val + 1, "HH").format("HH:mm");
       return (
@@ -31,7 +31,7 @@ export const NPMRDS_ATTRIBUTES = {
   },
   month: {
     values: [...Array.from({ length: 12 }, (_, i) => 0 + 1 + i)],
-    optionComp: ({val}) => {
+    optionComp: ({ val }) => {
       const months = [
         "January",
         "February",
@@ -48,7 +48,7 @@ export const NPMRDS_ATTRIBUTES = {
       ];
       return (
         <option className="ml-2  truncate" value={val}>
-          {months[(val-1)]}
+          {months[val - 1]}
         </option>
       );
     },
@@ -67,37 +67,40 @@ export const NPMRDS_ATTRIBUTES = {
   },
   counties: {
     values: [
-      34003,
-      // 34013,
-      // 34017,
-      // 34019,
-      // 34021,
-      // 34023,
-      // 34025,
-      // 34027,
-      // 34029,
-      // 34031,
-      // 34035,
-      // 34037,
-      // 34039,
-      // 36005,
-      // 36027,
-      // 36047,
-      // 36059,
-      // 36061,
-      // 36071,
-      // 36079,
-      // 36081,
-      // 36085,
-      // 36087,
-      // 36103,
-      // 36105,
-      // 36111,
-      // 36119,
-      // "09009",
-      // "09005",
-      // "09001",
+      36079, 36119, 36087, 36005, 36061, 36047, 36081, 36085, 36059, 36103,
     ],
+    // values: [
+    //   34003,
+    //   34013,
+    //   34017,
+    //   34019,
+    //   34021,
+    //   34023,
+    //   34025,
+    //   34027,
+    //   34029,
+    //   34031,
+    //   34035,
+    //   34037,
+    //   34039,
+    //   36005,
+    //   36027,
+    //   36047,
+    //   36059,
+    //   36061,
+    //   36071,
+    //   36079,
+    //   36081,
+    //   36085,
+    //   36087,
+    //   36103,
+    //   36105,
+    //   36111,
+    //   36119,
+    //   "09009",
+    //   "09005",
+    //   "09001",
+    // ],
     mapFilter: false,
   },
 };
